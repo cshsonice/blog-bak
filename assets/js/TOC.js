3ee9c39b640ba3fcc6a4c4f8a5172369f3ab958c
xander <26496749+cshsonice@users.noreply.github.com>
2018-10-03 16:46:34 +0800

    test

--- assets/js/TOC.js
+++ assets/js/TOC.js
@@ -1,3 +1,20 @@
 // 自动扫描文档，生成目录信息，显示在右侧栏
 
-alert('nihao')
+// 内容提取
+var mcontent = document.getElementById("main-content")  // content area
+var h1 = mcontent.getElementsByTagName("h1")
+var h2 = mcontent.getElementsByTagName("h2")
+var h3 = mcontent.getElementsByTagName("h3")
+var h4 = mcontent.getElementsByTagName("h4")
+var h5 = mcontent.getElementsByTagName("h5")
+
+
+// 侧边栏修改
+var sidebar = document.getElementById("sidebar")
+sidebar.innerHTML = ''  // clear sidebar
+
+console.log(h1)
+console.log(h2)
+console.log(h3)
+console.log(sidebar)
+
--- index.md
+++ index.md
@@ -36,10 +36,6 @@ qq:3526154926
 Email: <strong>3526154926@qq.com</strong>（常用）<br> or <strong>xanderchen98@gmail.com</strong>
 
 
-<script>
-  alert("hello!")
-</script>
-
 <script type="text/javascript" src="/assets/js/TOC.js"></script>
 [1]: python/index.md
 [2]: cpp/index.md
